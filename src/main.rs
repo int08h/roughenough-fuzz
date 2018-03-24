c8eccdfad0cc2263c0500f2143e40356b5574555
Stuart Stock <stuart@int08h.com>
2018-03-24 08:46:49 -0500

    Take crash case as command line argument

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,26 @@
 extern crate afl;
 extern crate roughenough;
 
+use std::fs::File;
+use std::io::prelude::*;
+
+use std::env;
+
 fn main() {
-    afl::read_stdio_bytes(|bytes| {
-        let _ = roughenough::RtMessage::from_bytes(&bytes);
-    });
+    let args = env::args();
+
+    if args.count() == 2 as usize {
+        let filename = env::args().nth(1).unwrap();
+        let mut file = File::open(filename).unwrap();
+        let size = file.metadata().unwrap().len();
+    
+        let mut contents = Vec::with_capacity(size as usize);
+        file.read_to_end(&mut contents).unwrap();
+    
+        let _ = roughenough::RtMessage::from_bytes(&contents);
+    } else {
+        afl::read_stdio_bytes(move |bytes| {
+            let _ = roughenough::RtMessage::from_bytes(&bytes);
+        });
+    }
 }
