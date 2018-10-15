299ae3f703a9aa8304cd13cd48300189142758f0
Aaron Hill <aa1ronham@gmail.com>
2018-10-14 20:23:34 -0400

    Use MemoryConfig

--- src/afl_server_target.rs
+++ src/afl_server_target.rs
@@ -6,11 +6,12 @@ use std::panic::AssertUnwindSafe;
 use roughenough::server::Server;
 
 fn main() {
-    let config = roughenough::config::make_config("ENV").unwrap();
+    // Don't conflict with a real server running on port 8687
+    let config = roughenough::config::MemoryConfig::new(8687);
 
     // We don't care about unwind safety,
     // afl.rs alwauys aborts on panic,
-    let server = AssertUnwindSafe(RefCell::new(Server::new(config)));
+    let server = AssertUnwindSafe(RefCell::new(Server::new(Box::new(config))));
 
     afl::fuzz(|bytes: &[u8]| {
         let mut borrow = server.borrow_mut();
